9da6a504afda848484af1c64b2389700e6cc161f
Broc Seib <broc.seib@gmail.com>
2025-05-29 12:27:54 -0400

    Provide reproduction of the bug
    Fix the jar dependencies needed.
    Provide a DDL file that can be used as a datasource.
    Setup a data source based on that DDL, and commit the .idea configs that
    represent it so that the bug can be reproduced by another person.

---
+++ Foo-DDL.sql
@@ -0,0 +1,5 @@
+create table foo
+(
+    foo_id   bigint primary key,
+    foo_name varchar(200) not null
+);
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jdbi:jdbi3-core:3.43.1")
+    val jdbiVersion = "3.36.0"
+    implementation("org.jdbi:jdbi3-core:${jdbiVersion}")
+    implementation("org.jdbi:jdbi3-sqlobject:${jdbiVersion}")
+    implementation("org.jdbi:jdbi3-kotlin:${jdbiVersion}")
+    implementation("org.jdbi:jdbi3-kotlin-sqlobject:${jdbiVersion}")
+    implementation("org.jdbi:jdbi3-postgres:${jdbiVersion}")
 }
 
 application {
